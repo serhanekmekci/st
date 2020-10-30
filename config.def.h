64ac4c2e9a2f697e078ed379700d21a46c2143d9
Serhan Ekmekçi <serhanekmekci@protonmail.com>
2020-10-30 22:34:31 +0300

    config

--- config.def.h
+++ config.def.h
@@ -203,6 +203,11 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ AltMask,              XK_K,           kscrollup,      {.i = 5} },
+	{ AltMask,              XK_J,           kscrolldown,    {.i = 5} },
+
 };
 
 /*
