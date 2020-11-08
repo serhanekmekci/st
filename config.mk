5d3ff939d8c8c576e7790fe5ad3736943a4c4fd5
Serhan Ekmekçi <serhanekmekci@protonmail.com>
2020-11-08 12:32:28 +0300

    solved

--- config.mk
+++ config.mk
@@ -16,13 +16,11 @@ PKG_CONFIG = pkg-config
 INCS = -I$(X11INC) \
        `$(PKG_CONFIG) --cflags fontconfig` \
        `$(PKG_CONFIG) --cflags freetype2` \
-	   #for ligatures patch to work with alpha
 	   `$(PKG_CONFIG) --libs harfbuzz`
 
 LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft -lXrender\
        `$(PKG_CONFIG) --libs fontconfig` \
        `$(PKG_CONFIG) --libs freetype2` \
-	   #for ligatures patch to work with alpha
 	   `$(PKG_CONFIG) --libs harfbuzz`
 
 # flags
