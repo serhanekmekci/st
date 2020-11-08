21a9f1ccdcad4052824a1c9c953f5116c2689fbf
Serhan Ekmekçi <serhanekmekci@protonmail.com>
2020-11-08 12:20:06 +0300

    ligatures support

--- config.mk
+++ config.mk
@@ -15,10 +15,15 @@ PKG_CONFIG = pkg-config
 # includes and libs
 INCS = -I$(X11INC) \
        `$(PKG_CONFIG) --cflags fontconfig` \
-       `$(PKG_CONFIG) --cflags freetype2`
+       `$(PKG_CONFIG) --cflags freetype2` \
+	   #for ligatures patch to work with alpha
+	   `$(PKG_CONFIG) --libs harfbuzz`
+
 LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft -lXrender\
        `$(PKG_CONFIG) --libs fontconfig` \
-       `$(PKG_CONFIG) --libs freetype2`
+       `$(PKG_CONFIG) --libs freetype2` \
+	   #for ligatures patch to work with alpha
+	   `$(PKG_CONFIG) --libs harfbuzz`
 
 # flags
 STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
