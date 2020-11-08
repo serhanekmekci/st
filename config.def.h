7442ce56252bc6eff356945d55767261f7fbe13e
Serhan Ekmekçi <serhanekmekci@protonmail.com>
2020-11-08 12:37:55 +0300

    alpha as xresources property

--- config.def.h
+++ config.def.h
@@ -202,6 +202,7 @@ ResourcePref resources[] = {
 		{ "borderpx",     INTEGER, &borderpx },
 		{ "cwscale",      FLOAT,   &cwscale },
 		{ "chscale",      FLOAT,   &chscale },
+		{ "alpha",        FLOAT,   &alpha },
 };
 
 /*
