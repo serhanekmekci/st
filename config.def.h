9a607cccf37c6b21464cd18707ce485b2b41029e
Serhan Ekmekçi <serhan@ekmekci.me>
2020-12-09 00:10:42 +0300

    font2 added

--- config.def.h
+++ config.def.h
@@ -6,6 +6,12 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Iosevka:style=Regular:size=12:antialias=true:autohint=true";
+
+/* Spare fonts */
+static char *font2[] = {
+	"Fira Code:style=Regular:size=12:antialias=true:autohint=true",
+};
+
 static int borderpx = 2;
 
 /*
@@ -74,7 +80,7 @@ static unsigned int cursorthickness = 2;
  * 0: disable (render all U25XX glyphs normally from the font).
  */
 const int boxdraw = 1;
-const int boxdraw_bold = 1;
+const int boxdraw_bold = 0;
 
 /* braille (U28XX):  1: render as adjacent "pixels",  0: use font */
 const int boxdraw_braille = 1;
